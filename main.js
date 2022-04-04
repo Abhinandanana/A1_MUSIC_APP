1c95a4e8de6df8dedb877cf46ba191283b8691d8
Abhinandanana <83591578+Abhinandanana@users.noreply.github.com>
2022-04-04 18:59:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+song1= "";
+song2= "";
+
+rightWristX= 0;
+rightWristY= 0;
+
+leftWristX= 0;
+leftWristY= 0;
+
+function preload(){
+    song1= loadSound("music.mp3");
+    song2= loadSound("music2.mp3");
+}
+
+function setup(){
+    canvas= createCanvas(600, 400);
+    canvas.center();
+
+    video= createCapture(VIDEO);
+    video.hide();
+
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is Loaded');
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        rightWristX= results[0].pose.rightWrist.x;
+        rightWristY= results[0].pose.rightWrist.y;
+
+        leftWristX= results[0].pose.leftWrist.x;
+        leftWristY= results[0].pose.leftWrist.y;
+    }
+}
+
+function draw(){
+    image(video, 0, 0, 600, 400);
+}
+
+function play(){
+    song1.play();
+    song1.setVolume(1);
+    song1.rate(1);
+}
